94db75f839bd9c4bf02dbac304ae0d4146e54055
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-11-15 14:56:04 +0100

    create app updated

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from api.extensions import db
 from api.auth import auth_blp
+from api.config import config_dict
 
 
-def create_app():
+def create_app(config_name='development'):
+
+    # set the configuration
+    config_class = config_dict[config_name]
+
     app = Flask(__name__)
     db.init_app(app)
 
