5a5d8b191757f49a41cbebc4d4d31d0d3aca7b9d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-11-15 15:23:18 +0100

    Create users.py

---
+++ api/models/users.py
@@ -0,0 +1,29 @@
+from api.extensions import db
+import secrets
+
+def hexid():
+    return secrets.token_hex(8)
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.String(100), primary_key=True, default=hexid)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
