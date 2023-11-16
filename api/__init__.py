325d22ce010a8d1fb2774c02f87d33ed6be2e114
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-11-16 15:23:31 +0100

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from api.extensions import db
+from api.extensions import db, migrate
 from api.auth import auth_blp
 from api.config import config_dict
 
 
-def create_app(config_name='development'):
-
-    # set the configuration
-    config_class = config_dict[config_name]
-
+def create_app(configure=config_dict["development"]):
     app = Flask(__name__)
+    app.config.from_object(configure)
+
     db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(auth_blp)
     return app
