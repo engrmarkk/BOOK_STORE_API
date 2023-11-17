2ff011c24e5a7ccb5fa70ac65717749027eae75e
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-11-17 15:56:12 +0100

    Update __init__.py

--- api/config/__init__.py
+++ api/config/__init__.py
@@ -15,11 +15,12 @@ class TestConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///book_api.db'
     SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 
 
 class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///book_api.db'
     DEBUG = True
 
 
