c44056646e3a7cd3fbc355ada2ba214f5f47d2a1
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-11-17 15:53:50 +0100

    Update __init__.py

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -9,30 +9,36 @@ auth_blp = Blueprint('auth', __name__)
 
 @auth_blp.route('/login')
 def login():
-    data = request.json
-    print(data, type(data), "data")
+    try:
+        data = request.json
+        print(data, type(data), "data")
 
-    email = data.get('email')
-    password = data.get('password')
+        email = data.get('email')
+        password = data.get('password')
 
-    if not email or not password:
+        if not email or not password:
+            return jsonify({"message": "All fields are required"}), 400
+
+        if not check_if_email_exists(email.lower()):
+            return jsonify({"message": "Email does not exist"}), 400
+
+        user = Users.query.filter_by(email=email.lower()).first()
+        if not sha256.verify(password, user.password):
+            return jsonify({"message": "Incorrect password"}), 400
+
+        access_token = create_access_token(identity=user.username)
+        refresh_token = create_refresh_token(identity=user.username)
+
+        return jsonify({
+            "message": "login successful",
+            "access_token": access_token,
+            "refresh_token": refresh_token
+        }), 200
+    except KeyError:
         return jsonify({"message": "All fields are required"}), 400
-
-    if not check_if_email_exists(email.lower()):
-        return jsonify({"message": "Email does not exist"}), 400
-
-    user = Users.query.filter_by(email=email.lower()).first()
-    if not sha256.verify(password, user.password):
-        return jsonify({"message": "Incorrect password"}), 400
-
-    access_token = create_access_token(identity=user.username)
-    refresh_token = create_refresh_token(identity=user.username)
-
-    return jsonify({
-        "message": "login successful",
-        "access_token": access_token,
-        "refresh_token": refresh_token
-    }), 200
+    except Exception as e:
+        print(e, "error")
+        return jsonify({"message": "An error occurred"}), 400
 
 
 @auth_blp.route('/register')
