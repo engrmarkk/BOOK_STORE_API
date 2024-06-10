51e8fb3aacc8c95d00baf9e96ecc389bfedff0c1
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-06-10 08:30:31 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,6 +1,12 @@
 from api import create_app
+import cProfile
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+    # app.run(debug=True)
+    # profiler.disable()
+    # profiler.print_stats(sort='time')
+    cProfile.run("app.run(debug=True)", "profiler")
